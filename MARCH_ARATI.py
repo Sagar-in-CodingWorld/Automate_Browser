344dfcdae39a7570e8da2e1109fd92c0f68e3ece
sagar <sagarascoder@gmail.com>
2024-02-16 13:04:19 +0530

    Successfully Working ,,,,yesterday 13 slots was booked.

--- MARCH_ARATI.py
+++ MARCH_ARATI.py
@@ -11,7 +11,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tkinter import *
-import customtkinter as ctk
 
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -25,7 +24,8 @@ SCHEDULE_URL = "http://reciprocal.wbhealth.gov.in/ScheduleDateForUser.aspx"
 I=10
 
 
-
+Id = '8670417524'
+Password = 'Arati@524'
 driver = webdriver.Chrome()
 #driver.implicitly_wait(10)
 driver.maximize_window()
@@ -33,103 +33,41 @@ driver.get(URL)
 
 Num = driver.find_element(By.ID,'txtUser')
 Pass = driver.find_element(By.ID,'txtPassword')
-April = [
-      
-      "//select/option[text()='02-Apr-2024']",
-      "//select/option[text()='03-Apr-2024']",
-      "//select/option[text()='04-Apr-2024']",
-      "//select/option[text()='11-Apr-2024']",
-      "//select/option[text()='16-Apr-2024']",
-      "//select/option[text()='17-Apr-2024']",
-      "//select/option[text()='18-Apr-2024']",
-      "//select/option[text()='23-Apr-2024']",
-      "//select/option[text()='24-Apr-2024']",
-      "//select/option[text()='25-Apr-2024']",
-      "//select/option[text()='30-Apr-2024']"
-      
-     ]
-March = [
-      
-      "//select/option[text()='05-Mar-2024']",
-      "//select/option[text()='06-Mar-2024']",
-      "//select/option[text()='07-Mar-2024']",
-      "//select/option[text()='12-Mar-2024']",
-      "//select/option[text()='13-Mar-2024']",
-      "//select/option[text()='14-Mar-2024']",
-      "//select/option[text()='19-Mar-2024']",
-      "//select/option[text()='20-Mar-2024']",
-      "//select/option[text()='21-Mar-2024']",
-      "//select/option[text()='27-Mar-2024']",
-      "//select/option[text()='28-Mar-2024']"
-      
-     ]
 
 #GUI WORK
-def send_user_data():
+def send_captcha():
     global CAP
-    global user
-    global password
-    global list_value
-    user = ID.get()
-    password = PASSWORD.get()
     CAP = captcha_entry.get()
-    list_value=list1.get()
-    print("The list value is :- ",list_value)
-    
-    print(user,password,CAP)
     window.destroy()
-
-
-
-window = ctk.CTk()
-window.title("User Input Window")
+window = Tk()
+window.title("Captcha Input Window")
 window.configure(bg="#A6E3E9")
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
 
 ScreenWidth = window.winfo_screenwidth()
 ScreenHeight = window.winfo_screenheight()
 
 WinWidth= int(ScreenWidth/2)
-WinHeight= int(ScreenHeight/2)
+WinHeight= int(ScreenHeight/3)
 
 #find the center position
 CenterX = int((ScreenWidth - WinWidth)/2)
 CenterY = int((ScreenHeight - WinHeight)/5)
-window.geometry(f"{WinWidth}x{WinHeight}+{CenterX}+{CenterY-70}")
-
-label1=ctk.CTkLabel(master=window,text="ENTER THE USER_ID")
-label1.place(x=100,y=30)
-ID=ctk.CTkEntry(master=window,width=200)
-ID.place(x=100,y=55)
-#ID.focus_set()
-
-label2=ctk.CTkLabel(master=window,text="ENTER THE PASSWORD")
-label2.place(x=100,y=100)
-PASSWORD=ctk.CTkEntry(master=window,width=200)
-PASSWORD.place(x=100,y=125)
-#PASSWORD.focus_set()
-
-label4=ctk.CTkLabel(master=window,text="Choose The Month")
-label4.place(x=400,y=100)
-list1=ctk.CTkOptionMenu(master=window,values=["April","March","CCC"],width=200)
-list1.place(x=400,y=125)
-#list_value=list1.get()
-
-
-label3=ctk.CTkLabel(master=window,text="ENTER THE CAPTCHA HERE")
-label3.place(x=100,y=190)
-captcha_entry=ctk.CTkEntry(master=window,width=100)
-captcha_entry.place(x=100,y=215)
-#captcha_entry.focus_set()
-
-b1=ctk.CTkButton(master=window,text="Submit Captcha",command = send_user_data,width = 500,height=100)
-b1.place(x=100,y=270)
+window.geometry(f"{WinWidth}x{WinHeight}+{CenterX}+{CenterY}")
+label=Label(window,text="Enter the captcha you can see on the browser")
+label.place(x=10,y=30)
+captcha_entry=Entry(window,width=100)
+captcha_entry.place(x=10,y=50)
+captcha_entry.focus_set()
+b1=Button(window,text="Submit Captcha",command = send_captcha ,width = 20,bg='green')
+b1.place(x=10,y=100)
 
 window.mainloop()
 
-Num.send_keys(user)
-Pass.send_keys(password)
+
+
+
+Num.send_keys(Id)
+Pass.send_keys(Password)
 
 '''cap = int(input("enter the captcha here"))#To take the captcha input'''
 driver.find_element(By.ID,'txtcaptcha').send_keys(CAP)#inserting the captcha 
@@ -146,13 +84,16 @@ drop = Select(x)
 
 #Select by Index
 drop.select_by_index(2)'''
- #global April
-
-#global March
-
-
-
-xx = list_value
+xx = [
+      
+      "//select/option[text()='05-Mar-2024']",
+      "//select/option[text()='06-Mar-2024']",
+      "//select/option[text()='07-Mar-2024']",
+      "//select/option[text()='12-Mar-2024']",
+      "//select/option[text()='13-Mar-2024']",
+      "//select/option[text()='14-Mar-2024']"      
+      
+     ]
 #measure the length of the date list
 date_len = len(xx)
 #select value from dropdown
@@ -224,7 +165,7 @@ while ( loop_condition ):
             sound.play()
             
             driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_btnSave').click()
-            print(" ###############  Available Slot   ############")
+            print(" ###############  Available Slot   ############ \n PROGRAM STOPPED \n Please Restart it again!!!!!!!")
             loop_condition = False
             #break
         
